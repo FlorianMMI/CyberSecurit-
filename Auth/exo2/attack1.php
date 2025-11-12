62da57162ddc072e86063b904cd9d784000bc84e
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-11-12 10:10:34 +0100

    Correction Attacj1

--- Auth/exo2/attack1.php
+++ Auth/exo2/attack1.php
@@ -2,4 +2,24 @@
 
 $letters = range('a', 'z');
 
+$ch = curl_init('http://localhost:3000/Auth/exo2/login1.php'); // use a full URL to avoid relative path issues
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); // don't follow redirects automatically
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // don't directly echo the response
 
+
+
+$post = [
+    'username' => 'student',
+    'password' => 'student'
+
+];
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+$response = curl_exec($ch);
+$url = curl_getinfo($ch, CURLINFO_REDIRECT_URL);
+
+echo $url . '/n';   
+
+curl_close($ch);
