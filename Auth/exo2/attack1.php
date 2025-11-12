60041ec55862342a44816f8d2bacb2c7931fa592
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-11-12 10:15:53 +0100

    Refactor login response handling and improve URL variable definitions

--- Auth/exo2/attack1.php
+++ Auth/exo2/attack1.php
@@ -8,7 +8,8 @@ curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); // don't follow redirects automatically
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // don't directly echo the response
 
-
+$url0 = 'http://localhost:3000/Auth/exo2/index.php';
+$url1 = 'http://localhost:3000/Auth/exo2/home.html';
 
 $post = [
     'username' => 'student',
@@ -20,6 +21,11 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 $response = curl_exec($ch);
 $url = curl_getinfo($ch, CURLINFO_REDIRECT_URL);
 
-echo $url . '/n';   
+if( $url === $url1){
+    echo "Login successful\n";
+} else {
+    echo "Login failed\n";
+}
+
 
 curl_close($ch);
